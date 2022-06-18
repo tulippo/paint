184da2b397367b7f7fff375c3d500a97418055fc
felippo <felippo.bianco@aln.senaicimatec.edu.br>
2022-06-18 14:16:34 -0300

    code

--- abrePaint.py
+++ abrePaint.py
@@ -1,7 +1,58 @@
 import pyautogui
+#pyautogui.press('winleft')
+#pyautogui.sleep(3)
+#pyautogui.write('paint')
+#pyautogui.sleep(5)
+#pyautogui.press('enter')
 
-pyautogui.press('winleft')
-pyautogui.sleep(3)
-pyautogui.write('paint')
-pyautogui.sleep(5)
-pyautogui.press('enter')
+##pyautogui.moveTo(140, 379)
+##pyautogui.dragTo(1092, 827, button='left')
+##952, 448
+##476, 224
+##1,6180
+def clicar():
+    pyautogui.moveTo(1500,450)
+    pyautogui.click(button='left')
+
+f20 = 20*1.6180
+f = 1.6180
+x = 600
+y = 600
+t = 1
+pyautogui.moveTo(x, y)
+pyautogui.sleep(t)
+pyautogui.dragTo(x+f20, y+20, button='left')
+clicar()
+
+pyautogui.moveTo(x, y+20)
+pyautogui.sleep(t)
+pyautogui.dragTo(x+f20, y+20+f20, button='left')
+clicar()
+
+pyautogui.moveTo(x+f20, y+20+f20)
+pyautogui.sleep(t)
+pyautogui.dragTo(x+20+2*f20, y, button='left')
+clicar()
+
+pyautogui.moveTo(x+20+2*f20, y)
+pyautogui.sleep(t)
+pyautogui.dragTo(x, y-20-2*f20, button='left')
+clicar()
+
+pyautogui.moveTo(x, y-20-2*f20)
+pyautogui.sleep(t)
+pyautogui.dragTo(x-70-2*f20, y+20+f20, button='left')
+clicar()
+
+pyautogui.moveTo(x-70-2*f20, y+20+f20)
+pyautogui.sleep(t)
+pyautogui.dragTo(x+20+2*f20, y+200+2*f20, button='left')
+clicar()
+
+pyautogui.moveTo(x+20+2*f20, y+200+2*f20)
+pyautogui.sleep(t)
+pyautogui.dragTo(x+320+2*f20, y-20-2*f20, button='left')
+clicar()
+
+
+
---
+++ fazFibonacci.py
@@ -0,0 +1,28 @@
+import pyautogui
+def clicar():
+    pyautogui.moveTo(1500,450)
+    pyautogui.click(button='left')
+x = 616
+y = 603
+
+n = int(input("valor de n: "))
+t1 = 0
+t2 = 1
+t3 = 1
+count = 1
+pyautogui.sleep(5)
+
+while count <= n:
+    pyautogui.moveTo(x, y)
+    print(t3)
+    x = x + t3*10
+    y = y + t3*10
+    pyautogui.sleep(2)
+    pyautogui.dragTo(x, y, button='left')
+    clicar()
+
+    t3 = t1 + t2
+    t1 = t2
+    t2 = t3
+    count+= 1
+    
