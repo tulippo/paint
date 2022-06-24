acf3aa8cb944e6ba8b446764f41af0c40049531d
felippo <felippo.bianco@aln.senaicimatec.edu.br>
2022-06-24 13:47:13 -0300

    code

--- circulo.py
+++ circulo.py
@@ -1,37 +1,27 @@
-import numpy as np
 import pyautogui as pyt
 import math as m
+
 X=960
 Y=540
-M = 1000
+n=0
+r = 103
+ang = 0
+
 
 pyt.sleep(2)
 pyt.moveTo(X,Y)
-pyt.dragTo(X,Y+100)
+pyt.dragTo(X,Y+100,button='left')
 pyt.sleep(2)
 pyt.moveTo(X,Y)
-pyt.dragTo(X+100,Y)
+pyt.dragTo(X+100,Y,button='left')
 pyt.sleep(2)
 
-angle = np.exp(1j *2 *np.pi/M)
-angles = np.cumprod(np.ones(M+1)*angle)
-x = np.real(angles)
-y = np.imag(angles)
-pyt.dragTo(X+ 30*x, Y + 30*y)
+#c1 = Circle(Point(X,Y),100)
+#print(c1.equation())
 
-count = 1
-somax = 0
-somay = 0
-
-
-#while count <= 90:
-#    xf = 90+somax
-#    yf = 0+somay
-#    xf = xi + xf
-#    yf = yi + yf
-#    pyt.moveTo(xi,yi)
-#    pyt.dragTo(xf,yf, button='left')
-#    count+=1
-#    somax+=-2
-#    somay+=1.15
-    
+while n <= 90:
+    cx = X + r*m.cos(m.radians(ang))
+    cy = Y + r*m.sin(m.radians(ang))
+    pyt.dragTo(cx,cy,button='left')
+    ang+=1
+    n+=1
